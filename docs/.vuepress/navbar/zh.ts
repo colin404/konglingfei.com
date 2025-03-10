0f044b53eccf380a58b459f4e735f098d841df64
孔令飞 <konglingfei.nosbelm@bytedance.com>
2025-03-10 22:16:53 +0800

    feat: commit at 20250310221653
    Change-Id: I51818ed6ece7d91ea31238495c7778eca0b89362

--- docs/.vuepress/navbar/zh.ts
+++ docs/.vuepress/navbar/zh.ts
@@ -18,6 +18,7 @@ export const zhNavbar = navbar([
         text: "Go 项目开发实战课",
         children: [
           { text: "Go 项目开发方法论课", icon: "methodology", link: "/cloudai/catalog/methodology.md" },
+          { text: "Go 项目开发极速入门课", icon: "newbie", link: "/cloudai/catalog/newbie.md" },
           { text: "Go 项目开发中级实战课", icon: "middle", link: "/cloudai/catalog/intermediate.md" },
           { text: "Go 项目开发专家级实战课", icon: "expert", link: "/cloudai/catalog/expert.md" },
         ],
@@ -61,7 +62,7 @@ export const zhNavbar = navbar([
       {
         text: "Go 项目",
         children: [
-          { text: "极速入门实战项目 newbie", icon: "chuji", link: "https://github.com/onexstack/newbie" },
+          { text: "极速入门实战项目 fastgo", icon: "chuji", link: "https://github.com/onexstack/fastgo" },
           { text: "初中级实战项目 miniblog", icon: "middle", link: "https://github.com/onexstack/miniblog" },
           { text: "中级实战项目 iam", icon: "middle", link: "https://github.com/marmotedu/iam" },
           { text: "专家级实战项目 onex", icon: "expert", link: "https://github.com/onexstack/onex" },
